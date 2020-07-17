3bf5d73189f5300e0bc235469cf4c8aaee7f4896
Charles Kantz <kantz.charles@gmail.com>
2020-07-17 13:17:15 -0700

    updating API address to be secure

--- src/CallAPI.js
+++ src/CallAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class CallAPI {
   static async searchShows(query) {
     try {
-      let res = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+      let res = await axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
       return res.data;
     } catch(err) {
       return new Error(err);
@@ -12,7 +12,7 @@ class CallAPI {
 
   static async getShowDetails(id) {
     try {
-      let res = await axios.get(`http://api.tvmaze.com/shows/${id}?embed=episodes`);
+      let res = await axios.get(`https://api.tvmaze.com/shows/${id}?embed=episodes`);
       return res.data;
     } catch(err) {
       return new Error(err);
